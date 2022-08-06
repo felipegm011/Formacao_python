a63fe3e4622f00591cb7819345704f8cdb13490b
Felipe Mendes <felipe.mendes@armazempb.com.br>
2022-08-06 00:46:06 -0300

    Primeiro curso formação python

--- jogos/jogo.py
+++ jogos/jogo.py
@@ -2,17 +2,20 @@
 import forca as forca
 import adivinhacao as adivinhacao
 
-print("***********************************")
-print("Escolha seu jogo")
-print("***********************************")
+def jogar():
+    print("***********************************")
+    print("Escolha seu jogo")
+    print("***********************************")
 
-print("(1) Forca (2) Advinhação")
+    print("(1) Forca (2) Advinhação")
 
-jogo = int(input("Qual jogo? "))
+    jogo = int(input("Qual jogo? "))
 
-if(jogo == 1):
-    print("forca")
-    forca.jogar_forca()
-elif(jogo == 2):
-    print("adivinhação")
-    adivinhacao.jogar_adivinhacao()
+    if(jogo == 1):
+        print("forca")
+        forca.jogar_forca()
+    elif(jogo == 2):
+        print("adivinhação")
+        adivinhacao.jogar_adivinhacao()
+if(__name__ == "__main__"):
+    jogar()
